14e47de8e78d471eb69c7d72edb26c425a0feb58
Tim Signore <timsignore@gmail.com>
2018-01-09 23:25:40 +0000

    Basic routing with react router v4.2 now working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import Board from './board';
+import { Switch, Route } from 'react-router-dom'
+import Welcome from './Welcome';
+import Board from './Board';
 
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <Board />
+      <app>
+        <Switch>
+          <Route exact path='/' component={Welcome} />
+          <Route path='/game' component={Board} />
+          
+        </Switch>
+      </app>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+// see https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
+import { BrowserRouter } from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
