6edd59484ff73f4d6d5dda52ca430deb645b8716
Tim Signore <timsignore@gmail.com>
2018-03-20 19:08:59 +0000

    Exit animation working

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './components/App';
 // see https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-
-console.log('render: ' + render, 'BrowserRouter: ' + BrowserRouter, 'Route: ' + Route);
 
 render((
     /************Change when pushing live****************/
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/pairs">
         <App />
     </BrowserRouter>
 ), document.getElementById('root'));
